154b107ae3fe0e83fa3740836671bc3652d98f90
Kostas <kostasspanolios@gmail.com>
2022-01-21 21:17:25 +0000

    api optimisations

--- src/Fetch.js
+++ src/Fetch.js
@@ -5,11 +5,13 @@ import DisplayApiData from "./DisplayApiData";
 import TextFieldComponent from "./TextFieldComponent";
 
 function Fetch() {
-  const [apiData, setApiData] = useState({});
+  const [apiData, setApiData] = useState({ api: [] });
   const [finalData, setFinalData] = useState();
   const [askData, setAskData] = useState();
   const [isSending, setIsSending] = useState(false);
   const isMounted = useRef(true);
+
+  console.log("rendered in Fetch.js ");
 
   const textFieldCallbacked = (params) => {
     setAskData(params);
@@ -20,8 +22,10 @@ function Fetch() {
       .then((response) => response.json())
       .then((data2) => {
         setFinalData(data2[0].meanings[0].definitions[0].definition);
+        //below works, but care bcs some wonky stuff happening with first call - calling education
+        // setApiData({ api: data2 });
+        // console.log(apiData.api.map((el) => el.word));
       });
-    //.then(modifyJson(apiData));
   };
 
   useEffect(() => {
