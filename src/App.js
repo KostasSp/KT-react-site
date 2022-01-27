5f119ec916dc0fab1cbf655ffbb62695813ad8c6
Kostas <kostasspanolios@gmail.com>
2022-01-27 11:34:09 +0000

    bg image

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
   const [currState, setCurrState] = useState(<MUIcomponent />); //FB people say not to use state for my static app
   const [askData, setAskData] = useState({});
   const [data, setData] = useState({ fdata: [] });
+  const imageUrl = `https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=628&q=80`;
   //add all components in this list, to keep it DRY
   const components = [<About />, <MUIcomponent />];
 
@@ -47,19 +48,24 @@ const App = () => {
 
   return (
     //add a message box somewhere, that can send message directly to her email
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: "url(" + imageUrl + ")",
+        backgroundSize: "cover",
+        overflow: "hidden",
+      }}
+    >
       <AppBar position="relative">
         <Toolbar>
           <NavBar AboutClick={aboutState} HomeClick={homeState} />
-          {/* check around min 33 in video, "useStyles", to (potentially) see how to align these */}
         </Toolbar>
       </AppBar>
       <div style={{ marginTop: "5rem" }}>
         <div>{currState} </div>
       </div>
       <div style={{ marginTop: "5rem" }}>
-        <Fetch callbackz={askData} />{" "}
-        {/* {apiData === null ? askData : apiData[0].word} */}
+        <Fetch callbackz={askData} />
       </div>
     </div>
   );
