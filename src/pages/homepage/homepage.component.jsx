1b0709de42ee260e272d3a8d5f7269978401b540
Kostas <kostasspanolios@gmail.com>
2022-02-01 01:38:21 +0000

    more component refactoring

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,56 @@
-import MUIcomponent from "../../MUIcomponent";
+// import MUIcomponent from "../../MUIcomponent";
+import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import { useEffect } from "react";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Container,
+  Button,
+} from "@material-ui/core";
 
-const HomePage = () => (
-  <div className="homepage">
-    <MUIcomponent />
-  </div>
-);
+const HomePage = () => {
+  useEffect(() => {
+    document.title = "Home";
+  }, []);
+  return (
+    <div className="homepage">
+      <CssBaseline />
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h3"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              <PhotoCamera />
+              Hello, I'm Kat
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              I'm a classical studies tutor, with a master's degree in online
+              teaching
+            </Typography>
+          </Container>
+          <Button variant="contained" disabled>
+            Disabled
+          </Button>
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default HomePage;
